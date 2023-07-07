62fc78c109ff2969b720deaa13e25ddf68b4e93b
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-07 18:13:37 +0200

    2

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,55 @@
+import { Link as RouterLink } from 'react-router-dom';
+
+import { Google } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography, Link } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title='Register'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Full name'
+              type='name'
+              placeholder='Your Name'
+              fullWidth
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='email@google.com'
+              fullWidth
+            ></TextField>
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='Your password'
+              fullWidth
+            ></TextField>
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant='contained' fullWidth>
+                Create Account
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}> Already an account?</Typography>
+            <Link component={RouterLink} color='inherit' to='/auth/login'>
+              Log in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,18 @@
-import { MailOutline } from '@mui/icons-material';
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant={'h1'}>JournalPage</Typography>
-      <MailOutline />
-    </>
-  )
+    <JournalLayout>
+      <Typography >
+        Ea Lorem culpa exercitation ullamco cupidatat fugiat eu. Elit laborum
+        consequat labore consectetur dolor ullamco cillum cillum magna irure ut.
+        Sint minim excepteur anim eu dolor eu. Tempor sit enim cupidatat ex
+        minim occaecat id tempor elit ut nulla et. Et labore dolor irure labore
+        ut elit in reprehenderit reprehenderit veniam mollit officia consequat.
+        Nulla mollit aliquip Lorem est pariatur veniam proident excepteur fugiat
+        sunt voluptate.
+      </Typography>
+    </JournalLayout>
+  );
 };
