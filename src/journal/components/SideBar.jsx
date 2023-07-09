83942cb75bff1bf8d60b1d434aa0a630686ed8df
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-09 12:20:05 +0200

    adding navbars

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,52 @@
+import { TurnedInNot } from '@mui/icons-material';
+import {
+  Box,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component='nav'
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant='permanent'
+        open
+        sx={{
+          display: { sx: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div'>
+            Josema
+          </Typography>
+        </Toolbar>
+        <List>
+          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText secondary={' Reprehenderit laboris et magna anim Lorem velit '} />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,13 +1,14 @@
 import { Box } from '@mui/material';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* Navbar */}
+      <NavBar drawerWidth={drawerWidth} />
 
-      {/* Sidebar */}
+      <SideBar drawerWidth={drawerWidth} />
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
         {/* toolbar */}
         {children}
