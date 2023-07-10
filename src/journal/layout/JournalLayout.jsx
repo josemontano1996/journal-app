577ca98c0eafca6c3d84f88c706e109e92e2e4bf
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-10 09:02:09 +0200

    added views

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
@@ -10,7 +10,7 @@ export const JournalLayout = ({ children }) => {
 
       <SideBar drawerWidth={drawerWidth} />
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-        {/* toolbar */}
+        <Toolbar/>
         {children}
       </Box>
     </Box>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,13 @@
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
+import { NoteViews } from '../views/NoteViews';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >
+      {/*    <Typography >
         Ea Lorem culpa exercitation ullamco cupidatat fugiat eu. Elit laborum
         consequat labore consectetur dolor ullamco cillum cillum magna irure ut.
         Sint minim excepteur anim eu dolor eu. Tempor sit enim cupidatat ex
@@ -12,7 +15,24 @@ export const JournalPage = () => {
         ut elit in reprehenderit reprehenderit veniam mollit officia consequat.
         Nulla mollit aliquip Lorem est pariatur veniam proident excepteur fugiat
         sunt voluptate.
-      </Typography>
+      </Typography> */}
+
+      <NothingSelectedView />
+      {/* <NoteViews /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
