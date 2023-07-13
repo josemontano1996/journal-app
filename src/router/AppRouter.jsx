d2f60e7624fc888107e863abdcd3f76be9158970
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-13 17:06:58 +0200

    added persistent auth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firebase/config';
+import { login, logout } from '../store/auth';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
-import { useSelector } from 'react-redux';
-import { CheckingAuth } from '../ui';
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  const { status } = useSelector(state => state.auth);
+  //para checkear el estado de la autenticacion en firebase
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
 
   if (status === 'checking') {
-    return <CheckingAuth />
+    return <CheckingAuth />;
   }
 
   return (
     <Routes>
-      <Route path='/auth/*' element={<AuthRoutes />} />
+      {status === 'authenticated' ? (
+        <Route path='/*' element={<JournalRoutes />} />
+      ) : (
+        <Route path='/auth/*' element={<AuthRoutes />} />
+      )}
 
-      <Route path='/*' element={<JournalRoutes />} />
+      <Route path='/*' element={<Navigate to='/aut/login' />} />
     </Routes>
   );
 };
