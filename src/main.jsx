5a99ed44a1f9dbdec71885be02ef5085330b335b
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-15 13:04:39 +0200

    fixing bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import './styles.css';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 import { JournalApp } from './JournalApp';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { store } from './store';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
-
