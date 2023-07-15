becc5f7836b4dd843972f2b09128a8a277eea77e
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-15 13:22:51 +0200

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+
+import './styles.css';
 
 import { JournalApp } from './JournalApp';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { store } from './store';
-import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <JournalApp />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
+
