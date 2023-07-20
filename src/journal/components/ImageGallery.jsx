12ed7188c08f59554cfc5831489faf1c65e86158
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-20 19:36:54 +0200

    fixed bug in images map

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,9 +1,9 @@
 import { ImageList, ImageListItem } from '@mui/material';
 
-export const ImageGallery = ({images}) => {
+export const ImageGallery = ({ images }) => {
   return (
-    <ImageList sx={{ width: '100%' , height: 500 }} cols={4} rowHeight={200}>
-      {images.map((image) => (
+    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
+      {images?.map(image => (
         <ImageListItem key={image}>
           <img
             src={`${image}?w=164&h=164&fit=crop&auto=format`}
